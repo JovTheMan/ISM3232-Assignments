35ee2560e0750ab0c0a4f7b41bb5f05906976b2b
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-08-29 19:32:13 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,7 +1,19 @@
 let employeeName = "Hugh Mungus"
 const employeeID = "69420"
-var isActive = true 
+var isActive = true  
 
 console.log(employeeName, typeof employeeName);
 console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
+
+
+let products = ["Galvanized Steel Cube", "Metal Alloy", "Steel Pipe"]
+const ProductDetails = {
+  name: "Galvanized Steel Cube",
+  price: 100,
+  inStock: true
+};
+
+console.log(products);
+console.log(ProductDetails);
+  

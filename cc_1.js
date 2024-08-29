6e8592d8c27936d717acb9436bc4c96600b11bbe
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-08-29 19:11:18 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "Hugh Mungus"
+const employeeID = "69420"
+var isActive = true 
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);

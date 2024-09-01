e16031568ba40680f005be64b55fc97a9b1e7e40
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-09-01 10:57:43 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+let employeeName = "George West";
+const employeeID = 12345;
+var isActive = true;
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
+

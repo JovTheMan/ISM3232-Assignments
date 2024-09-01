2831527d131ef72fc8bb9c53c600cbeb785455c3
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-09-01 11:14:48 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,14 @@ var isActive = true;
 console.log(employeeName, typeof employeeName);
 console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
+
+
+let products = ["iPhone", "Samsung", "Nokia"];
+const productDetails = {
+    name: "iPhone",
+    price: 999.99,
+    inStock: true
+};
+
+console.log(products);
+console.log(productDetails);

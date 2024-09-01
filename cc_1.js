bc7f74b4a54946717f58a33cc83f6b14c1d75fed
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-09-01 11:03:32 -0400

    Delete cc_1.js

--- cc_1.js
+++
@@ -1,8 +0,0 @@
-let employeeName = "George West";
-const employeeID = 12345;
-var isActive = true;
-
-console.log(employeeName, typeof employeeName);
-console.log(employeeID, typeof employeeID);
-console.log(isActive, typeof isActive);
-

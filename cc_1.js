26cc33f74c649b0ed3180af5f3827f2ad51b5383
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-09-01 10:37:34 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,18 @@
 let employeeName = "Jeff Bridges";
 const employeeID = 69420;
 var isActive = true;
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
+
+
+let products = ["salt", "pepper", "cuman"]
+const productDetails = {
+  name = "salt",
+  price = 1.00,
+  inStock = true
+};
+
+console.log(products);
+console.log(productDetails);
